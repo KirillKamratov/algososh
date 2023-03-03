8fe03380c450b76f2827be01ec40be136cb40dd3
Kirill Kamratov <kirill.kamratov@yandex.ru>
2023-03-03 19:47:08 +0300

    review fix 1

--- src/components/queue-page/queue-page.tsx
+++ src/components/queue-page/queue-page.tsx
@@ -21,14 +21,21 @@ export const QueuePage: React.FC = () => {
   const [ queue ] = useState(new Queue());
   const [ inputValue, setInputValue ] = useState<string>('')
   const [ array, setArray ] = useState(initialArr)
-  const disabled = false
+  const [ isLoading, setIsLoading ] = useState({
+    add: false,
+    delete: false,
+    clear: false,
+  });
+  const [ disabled, setDisabled ] = useState(false);
 
   const onChange = (event: ChangeEvent<HTMLInputElement>) => {
     setInputValue(event.target.value)
+    setDisabled(true)
   }
 
   const addItem = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault()
+    setIsLoading({...isLoading, add: true})
     const arr = [...array]
     const tail = queue.getTail()
     arr[tail].state = ElementStates.Changing
@@ -40,9 +47,13 @@ export const QueuePage: React.FC = () => {
     setInputValue('')
     arr[tail].state = ElementStates.Default
     setArray([...arr])
+    setIsLoading({...isLoading, add: false})
+    setDisabled(false)
   }
 
   const deleteItem = async () => {
+    setIsLoading({ ...isLoading, delete: true })
+    setDisabled(true)
     const arr = [...array]
     const head = queue.getHead()
     arr[head].state = ElementStates.Changing
@@ -54,12 +65,18 @@ export const QueuePage: React.FC = () => {
     setInputValue('')
     arr[head].state = ElementStates.Default
     setArray([...arr])
+    setIsLoading({ ...isLoading, delete: false })
+    setDisabled(false)
   }
 
   const clearQueue = () => {
+    setIsLoading({ ...isLoading, clear: true })
+    setDisabled(true)
     queue.clear()
     setArray(initialArr)
     setInputValue('')
+    setIsLoading({ ...isLoading, clear: false })
+    setDisabled(false)
   }
 
   return (
@@ -79,12 +96,14 @@ export const QueuePage: React.FC = () => {
           type={'submit'}
           linkedList={'small'}
           disabled={!inputValue || queue.isFull()}
+          isLoader={isLoading.add}
         />
         <Button
           text={'Удалить'}
           linkedList={'small'}
           disabled={disabled || queue.isEmpty()}
           onClick={deleteItem}
+          isLoader={isLoading.delete}
         />
         <Button
           extraClass={queueStyles.clear}
@@ -92,6 +111,7 @@ export const QueuePage: React.FC = () => {
           linkedList={'small'}
           disabled={disabled || queue.getTail() === 0}
           onClick={clearQueue}
+          isLoader={isLoading.clear}
         />
       </form>
       <ul className={queueStyles.listQueue}>
--- src/components/stack-page/stack-page.tsx
+++ src/components/stack-page/stack-page.tsx
@@ -16,7 +16,12 @@ export const StackPage: React.FC = () => {
   const [stack] = useState(new Stack<TArray>());
   const [ inputValue, setInputValue ] = useState('')
   const [ array, setArray ] = useState<TArray[]>([])
-  const disabled = false
+  const [isLoading, setIsLoading] = useState({
+    add: false,
+    delete: false,
+    clear: false,
+  });
+  const [disabled, setDisabled] = useState(false);
 
   const onChange = (event: ChangeEvent<HTMLInputElement>) => {
     setInputValue(event.target.value)
@@ -24,6 +29,8 @@ export const StackPage: React.FC = () => {
 
   const addItem = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault()
+    setIsLoading({ ...isLoading, add: true });
+    setDisabled(true);
     stack.push({ value: inputValue, state: ElementStates.Changing });
     setArray([...stack.getStack()]);
     setInputValue('');
@@ -33,9 +40,13 @@ export const StackPage: React.FC = () => {
       temp.state = ElementStates.Default;
     }
     setArray([...stack.getStack()]);
+    setIsLoading({ ...isLoading, add: false });
+    setDisabled(false);
   }
 
   const deleteItem = async () => {
+    setIsLoading({ ...isLoading, delete: true });
+    setDisabled(true);
     const temp = stack.peak()
     if (temp) {
       temp.state = ElementStates.Changing
@@ -44,11 +55,17 @@ export const StackPage: React.FC = () => {
     await delay(SHORT_DELAY_IN_MS)
     stack.pop()
     setArray([...stack.getStack()])
+    setIsLoading({ ...isLoading, delete: false });
+    setDisabled(false);
   }
 
   const clearStack = () => {
+    setIsLoading({ ...isLoading, clear: true });
+    setDisabled(true);
     stack.clear();
     setArray([...stack.getStack()]);
+    setDisabled(false);
+    setIsLoading({ ...isLoading, clear: false });
   }
 
   return (
@@ -68,12 +85,14 @@ export const StackPage: React.FC = () => {
           type={'submit'}
           linkedList={'small'}
           disabled={!inputValue}
+          isLoader={isLoading.add}
         />
         <Button
           text={'Удалить'}
           linkedList={'small'}
           disabled={disabled || array.length == 0}
           onClick={deleteItem}
+          isLoader={isLoading.delete}
         />
         <Button
           extraClass={stackStyles.clear}
@@ -81,6 +100,7 @@ export const StackPage: React.FC = () => {
           linkedList={'small'}
           disabled={disabled || array.length == 0}
           onClick={clearStack}
+          isLoader={isLoading.clear}
         />
       </form>
       <ul className={stackStyles.listStack}>
